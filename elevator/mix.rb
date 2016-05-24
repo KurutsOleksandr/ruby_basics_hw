834512716323368b6f80419d674da5e8e302717a
KurutsOleksandr <kinakataka@gmail.com>
2016-05-24 17:21:34 +0300

    just another commit

---
+++ elevator/mix.rb
@@ -0,0 +1,5 @@
+load 'elevator_move.rb'
+load 'people.rb'
+
+people(6) 
+elevator_move(5)
---
+++ elevator/people.rb
@@ -0,0 +1,11 @@
+
+def people(people)
+
+        if people >= 6 then
+            puts "motor 2 ON"
+            puts "In elevator #{people} people(s)"
+        else
+            puts "motor 2 OFF"
+            puts "In elevator #{people} people(s)"
+        end
+end
