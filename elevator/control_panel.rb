52bdbade0389c5cfb79bb04dc0e8816b4c8a7995
KurutsOleksandr <kinakataka@gmail.com>
2016-05-27 17:34:35 +0300

    Update file

--- elevator/control_panel.rb
+++ elevator/control_panel.rb
@@ -1,12 +1,38 @@
-load 'display_outside.rb'
 load 'motor.rb'
-
-    puts "How many people come?"
-    people = gets.to_i
-    puts "Choose floor"
-    elevator_floor = gets.to_i
-
-    display_outside = Display_outside.new
-    motor = Motors.new
-    motor.motors(people)
-    display_outside.display(people, elevator_floor)
+load 'display_outside.rb'
+load 'display_inside.rb'
+people = 0
+elevator_floor = 1
+puts "Write 'start' to enter command or 'exit' to close program"
+while (command = gets.chomp) != 'exit' 
+        display_outside = Display_outside.new
+        display_outside.display_status(people, elevator_floor)
+        if command == "start"
+            puts "How many people enter(exit)?"
+            people_come = gets.to_i
+            total_people = people + people_come
+            if  total_people <= 6 
+                if total_people >= 0
+                    people = total_people
+                else 
+                    puts "Some ghosts came with people from elevator. In elevator 0 people"
+                    people = 0
+                end
+            else
+                puts "too many people. In elevator 6 people"
+                people = 6
+            end
+                puts "Choose floor"
+            until (choose_floor = gets.chomp.to_i) <=11
+                puts "wrong floor. Please choose from 1 to 11"
+            end
+        end
+            elevator_floor = choose_floor
+            display_inside = Display_inside.new
+            motor = Motors.new
+            motor.motors(people)
+            display_outside.display(people, elevator_floor)
+            display_inside.displayInside(people, elevator_floor)
+        
+    
+end
