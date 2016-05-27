ba2276b4e2ac881596485013ebd753b74c49697b
KurutsOleksandr <kinakataka@gmail.com>
2016-05-27 15:19:37 +0300

    Delete elevator_move.rb

--- elevator/elevator_move.rb
+++
@@ -1,7 +0,0 @@
-    def elevator_move(elevator_floor)
-        if elevator_floor <= 0 or elevator_floor >=11 then
-            puts "Wrong floor"
-        else
-            puts "Elevator on the #{elevator_floor} floor"
-        end
-    end
