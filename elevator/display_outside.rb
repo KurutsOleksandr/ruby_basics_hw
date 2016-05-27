0a854490b675571ae8e5717bd35614a19c04f562
KurutsOleksandr <kinakataka@gmail.com>
2016-05-27 17:34:53 +0300

    Update file

--- elevator/display_outside.rb
+++ elevator/display_outside.rb
@@ -1,12 +1,19 @@
+
 class Display_outside
     attr_accessor :people, :elevator_floor
-    def initialize
-        people = 0
-        elevator_floor = 0
-    end
-    def display (people, elevator_floor)
+    def display_status (people, elevator_floor)
+        puts "++++++++++++++++++++++++"
         puts "In elevator #{people} people"
         puts "Elevator on #{elevator_floor} floor"
-       
+        puts "++++++++++++++++++++++++"
+    end
+    def display_motor(motor_2)
+        puts "=================="
+        puts "Motor 2 #{motor_2}"
+    end
+    def display (people, elevator_floor)
+        puts "#{people} people come(leave)"
+        puts "Elevator go to #{elevator_floor} floor"
+        puts "=================="
     end
 end
